f3e7bf4581978ddf774a24128280648380d8ae64
ZibyMax <max@rekstar-nn.ru>
2020-01-21 11:12:30 +0300

    ошибка в celery

--- getgoods/getgoods/celery.py
+++ getgoods/getgoods/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quick_publisher.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'getgoods.settings')
 celery_app = Celery('getgoods')
 celery_app.config_from_object('django.conf:settings')
 celery_app.autodiscover_tasks()
